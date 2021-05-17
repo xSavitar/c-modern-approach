8c1dceb7ced2a08fe13273f3fe94adb86c6f202c
Derick A <alangiderick@gmail.com>
2021-05-17 22:17:34 +0100

    Dimensional weight computation

---
+++ chpt2_dweight.c
@@ -0,0 +1,23 @@
+//
+// Created by Derick Alangi on 5/17/21.
+//
+
+/* compute the dimensional weight */
+
+#include <stdio.h>
+
+int main(void){
+    int height, length, width, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Dimensions: %d x %d x %d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+}
