83d6a73412ea9b3feec8373e4a3b65425effec55
Derick A <alangiderick@gmail.com>
2021-05-28 17:42:41 +0100

    Prints different conversion specifications to various formats

---
+++ chapter-3/tprintf.c
@@ -0,0 +1,20 @@
+//
+// Created by Derick Alangi on 5/28/21.
+//
+
+/* Prints int and float values in various formats */
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    float x;
+
+    i = 40;
+    x = 839.21f;
+
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+
+    return 0;
+}
