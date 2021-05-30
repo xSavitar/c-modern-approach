09823f9e2ad74d9d2bc7f4a0bb4f2d3201ddc127
Derick A <alangiderick@gmail.com>
2021-05-30 19:27:50 +0100

    Calculates the number of digits in an integer
    In addition, create a text file for chapter-1, it has no code.

---
+++ chapter-6/numdigits.c
@@ -0,0 +1,22 @@
+//
+// Created by Derick Alangi on 5/30/21.
+//
+
+/* Calculates the number of digits in an integer */
+#include <stdio.h>
+
+int main(void) {
+    int digits = 0, n;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+
+    do {
+        n /= 10;
+        digits++;
+    } while (n > 0);
+
+    printf("The number has %d digit(s).\n", digits);
+
+    return 0;
+}
