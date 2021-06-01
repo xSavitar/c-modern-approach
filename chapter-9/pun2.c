efa372c3aaed2f3f0f2d1919b3103abd53c01c70
Derick A <alangiderick@gmail.com>
2021-06-01 17:24:21 +0100

    A program (revised - using functions) to print a bad pun

---
+++ chapter-9/pun2.c
@@ -0,0 +1,16 @@
+//
+// Created by Derick Alangi on 6/1/21.
+//
+
+/* Prints a bad pun */
+
+#include <stdio.h>
+
+void print_pun(void) {
+    printf("To C, or not to C: that is the question.\n");
+}
+
+int main(void) {
+    print_pun();
+    return 0;
+}
